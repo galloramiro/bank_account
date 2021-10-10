58621f1fe7aadd395dfc1f297acdbe39c45a9b7c
ramiro <ramiro.gallo@coorva.com>
2021-10-10 19:11:46 -0300

    5 | Create the currency models and their endpoints
    - Create the the model
    - Create the serializer
    - Create the view set
    - Update the urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,9 +7,11 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 # Views
+from accounts.views import CurrencyViewSet
 
 
 router = DefaultRouter()
+router.register(r"currencies", CurrencyViewSet, basename="currencies")
 
 urlpatterns = [
     path("", include(router.urls))
